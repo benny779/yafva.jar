1a0f21d9cfb4d46610ffdae29036ce58d388af1c
Benny Shtemer <bs779779@gmail.com>
2025-04-08 12:18:45 +0300

    fix(#3): Change response bundle type to collection

--- src/main/java/il/co/outburn/rest/FhirValidator.java
+++ src/main/java/il/co/outburn/rest/FhirValidator.java
@@ -54,7 +54,7 @@ public class FhirValidator {
 
         // Result
         var responseBundle = new org.hl7.fhir.r4.model.Bundle();
-        responseBundle.setType(Bundle.BundleType.BATCHRESPONSE);
+        responseBundle.setType(Bundle.BundleType.COLLECTION);
 
         // Validates every entry individually
         int i = 0;
@@ -107,7 +107,7 @@ public class FhirValidator {
 
         // Result
         var responseBundle = new org.hl7.fhir.r5.model.Bundle();
-        responseBundle.setType(org.hl7.fhir.r5.model.Bundle.BundleType.BATCHRESPONSE);
+        responseBundle.setType(org.hl7.fhir.r5.model.Bundle.BundleType.COLLECTION);
 
         // Validates every entry individually
         int i = 0;
